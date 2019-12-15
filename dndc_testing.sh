9e8edcf7d48d7ce311fe9df23dc4021f4a66b466
Zufar <zufardhiyaulhaq@gmail.com>
2019-12-16 01:58:57 +0700

    automatic testing every scenario

---
+++ dndc_testing.sh
@@ -0,0 +1,38 @@
+echo << EOF > group_vars/all.yaml
+scenario: dndc
+# list avaliable: sndc, snsc, dndc, dnsc
+
+type: rrul
+# list available: rrul, tcp_download, tcp_upload
+
+ovnovs: "ovn"
+# list available: "ovn", "ovs"
+EOF
+
+ansible-playbook main.yml -i hosts/dndc
+
+echo << EOF > group_vars/all.yaml
+scenario: dndc
+# list avaliable: sndc, snsc, dndc, dnsc
+
+type: tcp_download
+# list available: rrul, tcp_download, tcp_upload
+
+ovnovs: "ovn"
+# list available: "ovn", "ovs"
+EOF
+
+ansible-playbook main.yml -i hosts/dndc
+
+echo << EOF > group_vars/all.yaml
+scenario: dndc
+# list avaliable: sndc, snsc, dndc, dnsc
+
+type: tcp_upload
+# list available: rrul, tcp_download, tcp_upload
+
+ovnovs: "ovn"
+# list available: "ovn", "ovs"
+EOF
+
+ansible-playbook main.yml -i hosts/dndc
